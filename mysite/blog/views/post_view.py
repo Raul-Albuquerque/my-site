b674cafe602255ee558eb7856fafe26dbb5d833e
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2024-03-04 23:31:19 -0300

    feat: template creation

--- mysite/blog/views/post_view.py
+++ mysite/blog/views/post_view.py
@@ -1,7 +1,13 @@
-from django.http import HttpResponse
 from django.views import generic
 
+from blog.models import Post
 
-class PostView(generic.View):
-    def get(self, request, *args, **kwargs):
-        return HttpResponse("Hello World")
+
+class PostView(generic.ListViewView):
+    queryset = Post.object.filter(status=1).order_by("_created_on")
+    template_name = "index.html"
+
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = "post_detail.html"
